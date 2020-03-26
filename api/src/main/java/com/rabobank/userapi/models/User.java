c349659d6f96d488a5b0aad8616337288ea8fd7b
Early Charlamagne <early@zed.network>
2020-03-26 11:32:16 +0100

    created model for user

---
+++ api/src/main/java/com/rabobank/userapi/models/User.java
@@ -0,0 +1,50 @@
+package com.rabobank.userapi.models;
+
+public class User {
+
+    private String firstname;
+    private String surname;
+    private String gender;
+    private String dateOfBirth;
+    private String picture;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
