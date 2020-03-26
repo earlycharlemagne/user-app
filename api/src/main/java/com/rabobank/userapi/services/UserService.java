d2e30d8ca8df158e887caca530c9249c206ef91f
Early Charlamagne <early@zed.network>
2020-03-26 11:55:13 +0100

    added service to retrieve user from stub server

---
+++ api/src/main/java/com/rabobank/userapi/services/UserService.java
@@ -0,0 +1,22 @@
+package com.rabobank.userapi.services;
+
+import com.rabobank.userapi.models.User;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class UserService {
+
+    private final static String ENDPOINT = "http://localhost:5000/users";
+    private RestTemplate restTemplate;
+
+    public UserService() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public User getUserByName(String firstname, String surname) {
+        String url = ENDPOINT + "?firstname=" + firstname + "&lastname=" + surname;
+
+        return restTemplate.getForObject(ENDPOINT, User.class);
+    }
+}
